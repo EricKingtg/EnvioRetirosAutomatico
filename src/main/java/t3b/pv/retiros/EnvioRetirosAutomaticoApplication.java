973365c7117f829aff2539b4f7ca81eecb1cc2ce
EricKingtg <ericktampicoxxi@hotmail.com>
2020-07-31 15:56:30 -0500

    Funcional

---
+++ src/main/java/t3b/pv/retiros/EnvioRetirosAutomaticoApplication.java
@@ -0,0 +1,13 @@
+package t3b.pv.retiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioRetirosAutomaticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioRetirosAutomaticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t3b/pv/retiros/EnvioRetirosAutomaticoApplicationTests.java
@@ -0,0 +1,13 @@
+package t3b.pv.retiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvioRetirosAutomaticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
